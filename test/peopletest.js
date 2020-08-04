239710d386c7c849537ecce066a006ae8324167a
Byko <bykocrypto@gmail.com>
2020-08-04 22:59:30 +0200

    test failed
    need to wrap the wei value as a string

--- test/peopletest.js
+++ test/peopletest.js
@@ -4,6 +4,6 @@ const truffleAssert = require("truffle-assertions");
 contract("People", async function(){
     it("shouldn't create a person with age over 150 years", async function(){
         let instance = await People.deployed();
-        await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei(1, "ether")}));
+        await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}));
     })
 });
