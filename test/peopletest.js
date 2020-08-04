af0b64755532943d4033da70497e0fb9916a6346
Byko <bykocrypto@gmail.com>
2020-08-04 22:56:26 +0200

    people test fail

---
+++ test/peopletest.js
@@ -0,0 +1,9 @@
+const People = artifacts.require("People");
+const truffleAssert = require("truffle-assertions");
+
+contract("People", async function(){
+    it("shouldn't create a person with age over 150 years", async function(){
+        let instance = await People.deployed();
+        await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei(1, "ether")}));
+    })
+});
