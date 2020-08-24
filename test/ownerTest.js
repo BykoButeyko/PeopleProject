cbc9bba3fc8fa7b7425516d9c87962287e39c229
Byko <bykocrypto@gmail.com>
2020-08-24 22:21:50 +0200

    new function to the contract test
    contract.deployed() --> conract.new()
    
    beforeEach();
    before();
    after();
    afterEach();

--- test/ownerTest.js
+++ test/ownerTest.js
@@ -1,27 +1,36 @@
 const People = artifacts.require("People");
 const truffleAssert = require("truffle-assertions");
 
-contract("People", async function(accounts){
-    it("Should set senior status properly", async ()=>{
+contract("People", async function (accounts) {
+    it("Should set senior status properly", async () => {
         let instance = await People.deployed();
-        await instance.createPerson("Barbara", 65, 171, {value: web3.utils.toWei("1", "ether"), from: accounts[0]});
+        await instance.createPerson("Barbara", 65, 171, {
+            value: web3.utils.toWei("1", "ether"),
+            from: accounts[0]
+        });
         let result = await instance.getPerson();
         assert(result.senior === true && result.age.toNumber() === 65, "Senior level not set");
     });
-    it("Sholudn't be deleted by an another account owner", async ()=>{
+    it("Sholudn't be deleted by an another account owner", async () => {
         let instance = await People.deployed();
-        await truffleAssert.fails(instance.deletePerson(accounts[0], {from: accounts[5]}),
-        truffleAssert.ErrorType.REVERT);
+        await truffleAssert.fails(instance.deletePerson(accounts[0], {
+                from: accounts[5]
+            }),
+            truffleAssert.ErrorType.REVERT);
     });
-    it("Should be able to delete your own account", async ()=>{
+    it("Should be able to delete your own account", async () => {
         let instance = await People.deployed();
         let person = await instance.getPerson();
-        await truffleAssert.passes(instance.deletePerson(accounts[5], {from: accounts[0]}))
-        assert(person.age.toNumber() !==5 && person.name !== '',
-        "A person has to have setted name and age before this test is executed");
-        await instance.deletePerson(accounts[0], {from: accounts[0]});
+        await truffleAssert.passes(instance.deletePerson(accounts[5], {
+            from: accounts[0]
+        }))
+        assert(person.age.toNumber() !== 0 && person.name !== '',
+            "A person has to have setted name and age before this test is executed");
+        await instance.deletePerson(accounts[0], {
+            from: accounts[0]
+        });
         person = await instance.getPerson();
         assert(person.age.toNumber() === 0 && person.name === '',
-        "Delete person was not executed correctly");
+            "Delete person was not executed correctly");
     });
 });
--- test/peopletest.js
+++ test/peopletest.js
@@ -1,51 +1,66 @@
 const People = artifacts.require("People");
 const truffleAssert = require("truffle-assertions");
 
-contract("People", async function () {
+contract("People", async (accounts) => {
+
+    let instance;
+
+    beforeEach(async () => {
+        instance = await People.deployed();
+    });
+
     it("shouldn't create a person with age over 150 years", async function () {
-        let instance = await People.deployed();
         await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {
             value: web3.utils.toWei("1", "ether")
         }), truffleAssert.ErrorType.REVERT);
     });
     it("shouldn't create a person without a payment", async function () {
-        let instance = await People.deployed();
         await truffleAssert.fails(instance.createPerson("Bob", 50, 190, {
             value: 1000
         }), truffleAssert.ErrorType.REVERT);
     });
     it("should set all people properties correctly", async function () {
-        let instance = await People.deployed();
         await instance.createPerson("Bob", 65, 190, {
             value: web3.utils.toWei("1", "ether")
         })
         let result = await instance.getPerson();
         assert(result.senior === true && result.age.toNumber() === 65, "Senior level not set");
     });
-    it("should set age correctly", async function(){
-        let instance = await People.deployed();
+    it("should set age correctly", async function () {
         let result = await instance.getPerson();
         assert(result.age.toNumber() === 65, "Age not set correctly");
     });
-});
-
-
-
-contract("People", async function(accounts){
-    it("Sholudn't be deleted by an another account owner", async ()=>{
-        let instance = await People.deployed();
-        await instance.createPerson("Bob", 65, 190, {value: web3.utils.toWei("1", "ether"), from: accounts[0]});
-        await truffleAssert.fails(instance.deletePerson(accounts[2], {from: accounts[2]}),
-        truffleAssert.ErrorType.REVERT);
+    it("Should set senior status properly", async () => {
+        await instance.createPerson("Barbara", 65, 171, {
+            value: web3.utils.toWei("1", "ether"),
+            from: accounts[0]
+        });
+        let result = await instance.getPerson();
+        assert(result.senior === true && result.age.toNumber() === 65, "Senior level not set");
     });
-    it("Should be able to delete your own account", async ()=>{
-        let instance = await People.deployed();
+    it("Sholudn't be deleted by an another account owner", async () => {
+        await truffleAssert.fails(instance.deletePerson(accounts[0], {
+                from: accounts[5]
+            }),
+            truffleAssert.ErrorType.REVERT);
+    });
+    it("Should be able to delete your own account", async () => {
+        let instance = await People.new();
+        await instance.createPerson("Bob", 65, 190, {
+            value: web3.utils.toWei("1", "ether"),
+            from: accounts[0]
+        });
         let person = await instance.getPerson();
-        assert(person.age.toNumber() !==4 && person.name !== '',
-        "A person has to have setted name and age before this test is executed");
-        await instance.deletePerson(accounts[0], {from: accounts[0]});
+        await truffleAssert.passes(instance.deletePerson(accounts[0], {
+            from: accounts[0]
+        }))
+        assert(person.age.toNumber() !== 0 && person.name !== '',
+            "A person has to have setted name and age before this test is executed");
+        await instance.deletePerson(accounts[0], {
+            from: accounts[0]
+        });
         person = await instance.getPerson();
         assert(person.age.toNumber() === 0 && person.name === '',
-        "Delete person was not executed correctly");
+            "Delete person was not executed correctly");
     });
 });
