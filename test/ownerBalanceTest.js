9cc6fba82768b0489c8517226baa3d7258de2db6
Byko <bykocrypto@gmail.com>
2020-08-27 23:03:23 +0200

    readable simplified test

--- test/ownerBalanceTest.js
+++ test/ownerBalanceTest.js
@@ -1,17 +1,27 @@
 const People = artifacts.require("People");
 const truffleAssert = require("truffle-assertions");
 
-contract("People", async function(accounts) {
+contract("People", async (accounts) => {
 
-    it("shouldn't delete person if not called by contract owner", async function() {
+    it("shouldn't delete person if not called by contract owner", async () => {
         let instance = await People.deployed();
-        await instance.createPerson("Kata", 29, 170, {value:web3.utils.toWei("1", "ether"), from:accounts[1]});
-        await truffleAssert.fails(instance.deletePerson(accounts[1], {from:accounts[1]}), truffleAssert.ErrorType.REVERT);
+        await instance.createPerson("Kata", 29, 170, {
+            value: web3.utils.toWei("1", "ether"),
+            from: accounts[1]
+        });
+        await truffleAssert.fails(instance.deletePerson(accounts[1], {
+            from: accounts[1]
+        }), truffleAssert.ErrorType.REVERT);
     });
 
-    it("should delete person if called by contract owner", async function() {
+    it("should delete person if called by contract owner", async () => {
         let instance = await People.deployed();
-        await instance.createPerson("Kata", 29, 170, {value:web3.utils.toWei("1", "ether"), from:accounts[1]});
-        await truffleAssert.passes(instance.deletePerson(accounts[1], {from:accounts[0]}));
+        await instance.createPerson("Kata", 29, 170, {
+            value: web3.utils.toWei("1", "ether"),
+            from: accounts[1]
+        });
+        await truffleAssert.passes(instance.deletePerson(accounts[1], {
+            from: accounts[0]
+        }));
     });
 })
