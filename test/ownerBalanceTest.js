248df4d11153abab55dc1e4d06fb504ba7308500
Byko <bykocrypto@gmail.com>
2020-08-27 23:37:44 +0200

    fix issues for ownerBalanceTest

--- test/ownerBalanceTest.js
+++ test/ownerBalanceTest.js
@@ -3,25 +3,38 @@ const truffleAssert = require("truffle-assertions");
 
 contract("People", async (accounts) => {
 
-    it("shouldn't delete person if not called by contract owner", async () => {
-        let instance = await People.deployed();
-        await instance.createPerson("Kata", 29, 170, {
-            value: web3.utils.toWei("1", "ether"),
-            from: accounts[1]
+    it("should increse the balance", async () => {
+        let instance = await People.new();
+        let balance = await web3.eth.getBalance(People.address);
+        await instance.createPerson("David", 21, 175, {
+            from: accounts[1],
+            value: web3.utils.toWei("1", "ether")
         });
-        await truffleAssert.fails(instance.deletePerson(accounts[1], {
-            from: accounts[1]
-        }), truffleAssert.ErrorType.REVERT);
+        let newBalance = await web3.eth.getBalance(People.address);
+        await truffleAssert.passes(newBalance > balance, "Transaction failed");
     });
 
-    it("should delete person if called by contract owner", async () => {
-        let instance = await People.deployed();
-        await instance.createPerson("Kata", 29, 170, {
-            value: web3.utils.toWei("1", "ether"),
-            from: accounts[1]
+    it("shouldnt be withdrawed by anyone but owner", async () => {
+        let instance = await People.new();
+        await instance.createPerson("Monica", 44, 166, {
+            from: accounts[1],
+            value: web3.utils.toWei("1", "ether")
         });
-        await truffleAssert.passes(instance.deletePerson(accounts[1], {
-            from: accounts[0]
+        await truffleAssert.fails(instance.withdrawAll({
+            from: accounts[1]
         }));
     });
-})
+
+    it("should be withdrawed by owner", async () => {
+        let instance = await People.new();
+        await instance.createPerson("Monica", 44, 166, {
+            from: accounts[1],
+            value: web3.utils.toWei("1", "ether")
+        });
+        await truffleAssert.passes(instance.withdrawAll({
+            from: accounts[0]
+        }));
+        let finalBalance = await web3.eth.getBalance(People.address);
+        await assert(parseInt(finalBalance) === 0, "Withdraw failed");
+    });
+});
